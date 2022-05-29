8501e8d8ea12250195d3dc4934c238cca6dd2883
Charan Kumar Ganji <charan_kumar_ganji@epam.com>
2022-05-29 18:00:33 +0530

    Task 5.4 Added

---
+++ NodeJS/5.4/src/routes/router.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import controller from '../controller/controller';
+import { validateUser } from '../middleware/validateUser';
+
+export const router = Router();
+
+router.get('/getUser/:id', controller.getUser);
+router.post('/createUser', validateUser, controller.createUser);
+router.delete('/removeUser/:id', controller.removeUser);
+router.put('/updateUser/:id', validateUser, controller.updateUser);
+router.get('/getAutoSuggestUsers/:loginSubstring/:limit', controller.autoSuggest);
+router.get('/getAllUsers', controller.getAllUsers);
+router.use('*', (req, res) => {
+    res.status(404).send({
+        message: 'Page not found'
+    });
+});
