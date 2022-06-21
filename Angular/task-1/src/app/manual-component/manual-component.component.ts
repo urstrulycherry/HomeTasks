0b9cb64eed1c129ed6d50844220a994d83aaf66d
Charan Kumar Ganji <charan_kumar_ganji@epam.com>
2022-06-21 20:22:59 +0530

    Basic angular task added

---
+++ Angular/task-1/src/app/manual-component/manual-component.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-manual-component',
+    templateUrl: './manual-component.component.html',
+    styleUrls: ['./manual-component.component.css']
+})
+export class ManualComponentComponent {
+}
